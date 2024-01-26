07781dc73ede07d33024b54b7a7b258449a95959
liubovridko <liubovusikova1@gmail.com>
2024-01-26 21:13:06 +0200

    added footer styles

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,15 @@
-import * as flsFunctions from './modules/functions.js';
+import * as flsFunctions from './modules/functions.js'
 
-flsFunctions.isWebp();
+flsFunctions.isWebp()
 /*module for burger menu*/
-flsFunctions.toogleMenu();
+flsFunctions.toogleMenu()
 /*module "Spollers"*/
-//flsFunctions.spollers();
-import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+flsFunctions.spollers()
+import Swiper from 'swiper'
+import { Navigation, Pagination } from 'swiper/modules'
 
 // init Swiper:
 const swiper = new Swiper('.swiper', {
   // configure Swiper to use modules
   modules: [Navigation, Pagination],
-});
+})
--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -3,32 +3,32 @@
 export function isWebp() {
   //check support
   function testWebp(callback) {
-    let webp = new Image();
+    let webp = new Image()
     webp.onload = webp.onerror = function () {
-      callback(webp.height == 2);
-    };
+      callback(webp.height == 2)
+    }
     webp.src =
-      'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+      'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA'
   }
   //add class _webp or _no-webp in HTML
   testWebp(function (support) {
-    let classname = support === true ? 'webp' : 'no-webp';
-    document.documentElement.classList.add(classname);
-  });
+    let classname = support === true ? 'webp' : 'no-webp'
+    document.documentElement.classList.add(classname)
+  })
 }
 
 /*toggle menu buger*/
 
 export function toogleMenu() {
-  var burgerIcon = document.getElementById('burgerIcon');
-  var menu = document.getElementById('menu');
+  var burgerIcon = document.getElementById('burgerIcon')
+  var menu = document.getElementById('menu')
 
   // Добавляем обработчик события для клика на бургер-иконке
   burgerIcon.addEventListener('click', function () {
     // Переключаем класс "open" для меню
-    menu.classList.toggle('open');
-    burgerIcon.classList.toggle('menu-open');
-  });
+    menu.classList.toggle('open')
+    burgerIcon.classList.toggle('menu-open')
+  })
 
   // Добавляем обработчик события для закрытия меню при клике вне его области на маленьких экранах
   document.addEventListener('click', function (event) {
@@ -37,8 +37,199 @@ export function toogleMenu() {
       !burgerIcon.contains(event.target) &&
       !menu.contains(event.target)
     ) {
-      menu.classList.remove('open');
-      burgerIcon.classList.remove('menu-open');
+      menu.classList.remove('open')
+      burgerIcon.classList.remove('menu-open')
     }
-  });
+  })
+}
+
+export function spollers() {
+  //Spollers
+  const spollersArray = document.querySelectorAll('[data-spollers]')
+  if (spollersArray.length > 0) {
+    //получение обычных спойлеров
+    const spollersRegular = Array.from(spollersArray).filter(function (item, index, self) {
+      return !item.dataset.spollers.split(',')[0]
+    })
+
+    //инициализаця обычных слайдеров
+    if (spollersRegular.length > 0) {
+      initSpollers(spollersRegular)
+    }
+
+    // получение слайдеров с медиа запросом
+    const spollersMedia = Array.from(spollersArray).filter(function (item, index, self) {
+      return item.dataset.spollers.split(',')[0]
+    })
+
+    //инициализация слайдеров с медиа запросом
+    if (spollersMedia.length > 0) {
+      const breakpointsArray = []
+      spollersMedia.forEach((item) => {
+        const params = item.dataset.spollers
+        const breakpoint = {}
+        const paramsArray = params.split(',')
+        breakpoint.value = paramsArray[0]
+        breakpoint.type = paramsArray[1] ? paramsArray[1].trim() : 'max'
+        breakpoint.item = item
+        breakpointsArray.push(breakpoint)
+      })
+
+      //Получаем уникальные брекпоинты
+      let mediaQueries = breakpointsArray.map(function (item) {
+        return '(' + item.type + '-width: ' + item.value + 'px),' + item.value + ',' + item.type
+      })
+      mediaQueries = mediaQueries.filter(function (item, index, self) {
+        return self.indexOf(item) === index
+      })
+
+      //Работаем с каждым брикпоинтом
+      mediaQueries.forEach((breakpoint) => {
+        const paramsArray = breakpoint.split(',')
+        const mediaBreakpoint = paramsArray[1]
+        const mediaType = paramsArray[2]
+        const matchMedia = window.matchMedia(paramsArray[0])
+
+        //Обекты с нужними условиями
+        const spollersArray = breakpointsArray.filter(function (item) {
+          if (item.value === mediaBreakpoint && item.type === mediaType) {
+            return true
+          }
+        })
+        //Cобытия
+        matchMedia.addListener(function () {
+          initSpollers(spollersArray, matchMedia)
+        })
+        initSpollers(spollersArray, matchMedia)
+      })
+    }
+
+    //Инициализация
+    function initSpollers(spollersArray, matchMedia = false) {
+      spollersArray.forEach((spollersBlock) => {
+        spollersBlock = matchMedia ? spollersBlock.item : spollersBlock
+        if (matchMedia.matches || !matchMedia) {
+          spollersBlock.classList.add('_init')
+          initSpollerBody(spollersBlock)
+          spollersBlock.addEventListener('click', setSpollerAction)
+        } else {
+          spollersBlock.classList.remove('_init')
+          initSpollerBody(spollersBlock, false)
+          spollersBlock.removeEventListener('click', setSpollerAction)
+        }
+      })
+    }
+
+    //Робота с контентом
+    function initSpollerBody(spollersBlock, hideSpollerBody = true) {
+      const spollerTitles = spollersBlock.querySelectorAll('[data-spoller]')
+      if (spollerTitles.length > 0) {
+        spollerTitles.forEach((spollerTitle) => {
+          if (hideSpollerBody) {
+            spollerTitle.removeAttribute('tabindex')
+            if (!spollerTitle.classList.contains('_active')) {
+              spollerTitle.nextElementSibling.hidden = true
+            }
+          } else {
+            spollerTitle.setAttribute('tabindex', '-1')
+            spollerTitle.nextElementSibling.hidden = false
+          }
+        })
+      }
+    }
+
+    //Выполняется когда мы кликаем на стрелку либо заголовок
+    function setSpollerAction(e) {
+      const el = e.target
+      if (el.hasAttribute('data-spoller') || el.closest('[data-spoller]')) {
+        const spollerTitle = el.hasAttribute('data-spoller') ? el : el.closest('[data-spoller]')
+        const spollersBlock = spollerTitle.closest('[data-spollers]')
+        const oneSpoller = spollersBlock.hasAttribute('data-one-spoller') ? true : false
+        //Условие нужно ли добавлять акардеон
+        if (!spollersBlock.querySelectorAll('._slide').length) {
+          if (oneSpoller && !spollerTitle.classList.contains('_active')) {
+            hideSpollersBody(spollersBlock)
+          }
+          spollerTitle.classList.toggle('_active')
+          _slideToggle(spollerTitle.nextElementSibling, 500)
+        }
+        e.preventDefault()
+      }
+    }
+    function hideSpollersBody(spollersBlock) {
+      const spollerActiveTitle = spollersBlock.querySelector('[data-spoller]._active')
+      if (spollerActiveTitle) {
+        spollerActiveTitle.classList.remove('_active')
+        _slideUp(spollerActiveTitle.nextElementSibling, 500)
+      }
+    }
+  }
+  /*----------------------------------------*/
+  // SlideToggle
+  // Анимирует скрытие
+  let _slideUp = (target, duration = 500) => {
+    target.style.transitionProperty = 'height, margin, padding'
+    target.style.transitionDuration = duration + 'ms'
+    target.style.height = target.offsetHeight + 'px'
+    target.offsetHeight
+    target.style.overflow = 'hidden'
+    target.style.height = 0
+    target.style.paddingTop = 0
+    target.style.paddingBottom = 0
+    target.style.marginTop = 0
+    target.style.marginBottom = 0
+    window.setTimeout(() => {
+      target.style.display = 'none'
+      target.style.removeProperty('height')
+      target.style.removeProperty('padding-top')
+      target.style.removeProperty('padding-bottom')
+      target.style.removeProperty('margin-top')
+      target.style.removeProperty('margin-bottom')
+      target.style.removeProperty('overflow')
+      target.style.removeProperty('transition-duration')
+      target.style.removeProperty('transition-property')
+      target.classList.remove('_slide')
+    }, duration)
+  }
+  // Анимирует показ
+  let _slideDown = (target, duration = 500) => {
+    target.style.removeProperty('display')
+    let display = window.getComputedStyle(target).display
+    if (display === 'none') display = 'block'
+
+    target.style.display = display
+    let height = target.offsetHeight
+    target.style.overflow = 'hidden'
+    target.style.height = 0
+    target.style.paddingTop = 0
+    target.style.paddingBottom = 0
+    target.style.marginTop = 0
+    target.style.marginBottom = 0
+    target.offsetHeight
+    target.style.transitionProperty = 'height, margin, padding'
+    target.style.transitionDuration = duration + 'ms'
+    target.style.height = height + 'px'
+    target.style.removeProperty('padding-top')
+    target.style.removeProperty('padding-bottom')
+    target.style.removeProperty('margin-top')
+    target.style.removeProperty('margin-bottom')
+    window.setTimeout(() => {
+      target.style.removeProperty('height')
+      target.style.removeProperty('overflow')
+      target.style.removeProperty('transition-duration')
+      target.style.removeProperty('transition-property')
+      target.classList.remove('_slide')
+    }, duration)
+  }
+  // Комдинация двох функций
+  let _slideToggle = (target, duration = 500) => {
+    if (!target.classList.contains('_slide')) {
+      target.classList.add('_slide')
+      if (window.getComputedStyle(target).display === 'none') {
+        return _slideDown(target, duration)
+      } else {
+        return _slideUp(target, duration)
+      }
+    }
+  }
 }
